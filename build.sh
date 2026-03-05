64a0d69f1b21d8659fbdf10b2bb38ae0c7410ed1
JhonierMejia10 <124406476+JhonierMejia10@users.noreply.github.com>
2026-03-04 22:30:46 -0500

    Crear super usuario, solución

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 
 python manage.py migrate
 
+python manage.py createsuperuser --noinput || true
