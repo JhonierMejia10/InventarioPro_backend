be02177710ccd6e94d24be4087b7c002ee2ba28f
JhonierMejia10 <124406476+JhonierMejia10@users.noreply.github.com>
2026-03-04 21:05:04 -0500

    Configuración de despliegue

--- gestion_inventarios/deployment_settings.py
+++ gestion_inventarios/deployment_settings.py
@@ -1,17 +1,16 @@
 import os 
 import dj_database_url
-from .settings import *
+from .settings import * 
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
-
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -22,23 +21,54 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-#CORS_ALLOWED_ORIGINS = [
-
-#]
+CORS_ALLOWED_ORIGINS = [
+    'https://render-deploy-tutorial-reactjs-code.onrender.com'
+]
 
 STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage"
+    "default":{
+        "BACKEND" : "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    }
+        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+
 }
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get['DATABASE_URL'],
-        conn_max_age=600, 
-        conn_health_checks=True
+        default= os.environ['DATABASE_URL'], 
+        conn_max_age=600
     )
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
+
+ADMINS = [("CBI Analytics", "YOUREMAIL@EMAIL.com")]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = 'default from email'
